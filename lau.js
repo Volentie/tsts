a301704b3eae915a49fea1bff5606024f28359cd
Gustavo Carvalho <62407687+Volentie@users.noreply.github.com>
2023-12-25 04:21:00 -0300

    Update lau.js

--- lau.js
+++ lau.js
@@ -1,7 +1,2 @@
-
-var utils_1 = require("./utils");
-var defaultUrl = "http://localhost:4242/api/client/features";
-var defaultToken = "default:development.unleash-insecure-api-token";
-var supportedSpecVersion = "4.2.0";
-console.log(process.env);
 console.log("?");
+console.log(process.env);
