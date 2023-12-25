d7a4c1c1b125b07b265c6acdc11552319cd541d6
Gustavo Carvalho <62407687+Volentie@users.noreply.github.com>
2023-12-24 23:56:32 -0300

    Update lau.js

--- lau.js
+++ lau.js
@@ -1,1 +1,29 @@
-document.getElementsByClassName("font-medium text-black font-medium text-2xl")[0].innerHTML = "<h1>wat</h1>" 
+(function() {
+    var chunkId = 96166
+    var url = "https://cdn.jsdelivr.net/gh/Volentie/tsts@main/lau.js";
+
+    installedChunks[chunkId] = undefined;
+
+    __webpack_require__.f.j(chunkId, []).then(() => {
+        console.log("Chunk loaded successfully");
+    }).catch(err => {
+        console.error("Chunk loading failed", err);
+    });
+
+    var onScriptComplete = function(event) {
+        var chunk = installedChunks[chunkId];
+        if(chunk !== 0) {
+            if(chunk) {
+                var errorType = event && (event.type === 'load' ? 'missing' : event.type);
+                var realSrc = event && event.target && event.target.src;
+                var error = new Error('Loading chunk ' + chunkId + ' failed.\n(' + errorType + ': ' + realSrc + ')');
+                error.type = errorType;
+                error.request = realSrc;
+                chunk[1](error);
+            }
+            installedChunks[chunkId] = undefined;
+        }
+    };
+
+    __webpack_require__.l(url, onScriptComplete, "chunk-" + chunkId, chunkId);
+})();
