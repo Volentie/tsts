2e7dc88119a99d07e57f00fa39632a50259ed920
Gustavo Carvalho <62407687+Volentie@users.noreply.github.com>
2023-12-25 04:19:50 -0300

    Update lau.js

--- lau.js
+++ lau.js
@@ -1,5 +1,4 @@
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getDefinitions = exports.getDefaultConfig = void 0;
+
 var utils_1 = require("./utils");
 var defaultUrl = "http://localhost:4242/api/client/features";
 var defaultToken = "default:development.unleash-insecure-api-token";
